0ad203c8fe55426b8cc0527388242743d0a8f380
vinic_000 <vinic_000@192.168.1.148>
2016-05-20 09:19:52 +0200

    init EHS6 Java ME Project

---
+++ ehs6/mqtt/src/things/Surfboard.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package things;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.microedition.io.CommConnection;
+import javax.microedition.io.Connector;
+
+/**
+ *
+ * @author vsenger
+ */
+public class Surfboard {
+
+    static CommConnection commConn;
+    static InputStream inStream;
+    static OutputStream outStream;
+
+    public static String sensors() throws IOException {
+        String strResponse = null;
+        System.out.println("Available COM-Ports: " + System.getProperty("microedition.commports"));
+        String strCOM = "comm:COM0;blocking=on;baudrate=9600";
+        commConn = (CommConnection) Connector.open(strCOM);
+        System.out.println("CommConnection(" + strCOM + ") opened");
+        System.out.println("Real baud rate: " + commConn.getBaudRate());
+        inStream = commConn.openInputStream();
+        outStream = commConn.openOutputStream();
+        String sending = "sensors";
+        System.out.println("Sending discovery to surfboard...");
+        outStream.write(sending.getBytes());
+        byte response[] = new byte[1024];
+        int counter = 0;
+        try {
+            System.out.println("Waiting responde RXTX...");
+            for (int x = 0; x < 10; x++) {
+                Thread.sleep(40);
+                while (inStream.available() > 0) {
+                    response[counter++] = (byte) inStream.read();
+                }
+            }
+            System.out.println("Response " + (counter - 1) + " bytes..");
+            strResponse = new String(response);
+            System.out.println(strResponse);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        return strResponse;
+    }
+
+}
